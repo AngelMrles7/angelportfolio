2b0019779cdf4ed3bd38f51bd3116fa4605a2736
Angel Morales Chocue <angelmrles7@gmail.com>
2023-05-04 17:13:17 -0500

    Added new styles and effects to the text.

--- src/components/layout/home/Home.tsx
+++ src/components/layout/home/Home.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 
 const Home = () => {
   return (
-    <section className="section-home">
-      <div className="section-home__info">
-        <h1 className="section-home__info__title">
-          <span>Hello! 👋</span>
-          <span>I'm Angel Morales</span>
-          <span>Web developer</span>
+    <section className="home-section" id="home">
+      <div className="home-section__info">
+        <h1 className="home-section__info__title">
+          <Fade cascade damping={1} triggerOnce={true}>
+            <span>Hello! 👋</span>
+            <span className="css-1c2fuzs--effect-text">I'm Angel Morales</span>
+            <span className="css-1c2fuzs__font-size">Web developer</span>
+          </Fade>
         </h1>
-        <div className="section-home__info__btns">
+
+        <div className="home-section__info__btns">
           <button className="button download">
             <span>Download CV</span>
           </button>
         </div>
       </div>
-      <div className="section-home__image">
+      <div className="home-section__image">
         <img src="#" alt="" />
       </div>
     </section>
