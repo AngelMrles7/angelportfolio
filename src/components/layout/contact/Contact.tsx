918f383d2047ade31b6422f99639a5435fb912a7
Angel Morales Chocue <angelmrles7@gmail.com>
2023-05-10 19:09:29 -0500

    Responsive has been added to the form.

--- src/components/layout/contact/Contact.tsx
+++ src/components/layout/contact/Contact.tsx
@@ -2,30 +2,30 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <section className="section-contact">
-      <div className="section-contact__form-box">
-        <h2 className="section-contact__title">Contact me</h2>
+    <section className="contact-section" id="contact">
+      <div className="contact-section__form-box">
+        <h2 className="contact-section__title">Contact me</h2>
 
-        <form className="section-contact__form">
-          <div className="section-contact__form__input-box">
+        <form className="contact-section__form">
+          <div className="contact-section__form__input-box">
             <input type="text" name="name" required />
             <label>Name</label>
           </div>
-          <div className="section-contact__form__input-box">
+          <div className="contact-section__form__input-box">
             <input type="email" name="email" required />
             <label>Email</label>
           </div>
-          <div className="section-contact__form__input-box">
+          <div className="contact-section__form__input-box">
             <input type="text" name="subject" required />
             <label>Subject</label>
           </div>
-          <div className="section-contact__form__textarea-box">
+          <div className="contact-section__form__textarea-box">
             <textarea name="message" placeholder="Message"></textarea>
           </div>
-          <div className="section-contact__form__button-box">
+          <div className="contact-section__form__button-box">
             <button
               type="submit"
-              className="section-contact__form__button-box__button btn-send"
+              className="contact-section__form__button-box__button btn-send"
             >
               <span className=""> Send message</span>
               <svg>
