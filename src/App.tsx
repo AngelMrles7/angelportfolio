017b1b44dc6acf9de29a71cfa6473a731a882268
Angel Morales Chocue <angelmrles7@gmail.com>
2023-06-06 22:04:18 -0500

    The context that surrounds the other components has been added so that they can access it.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import About from "./components/layout/about/About";
 import Contact from "./components/layout/contact/Contact";
 import Home from "./components/layout/home/Home";
@@ -5,18 +6,25 @@ import Navbar from "./components/layout/nav/Navbar";
 import Projects from "./components/layout/projects/Projects";
 import Skills from "./components/layout/skills/Skills";
 import PanelButton from "./components/ui/Panel_Button";
+import { LanguageProvider } from "./context/language/LanguageState";
 
 function App() {
+  const [showPanel, setShowPanel] = useState(false);
+
+  const updateState = () => {
+    setShowPanel(!showPanel);
+  };
+
   return (
-    <>
-      <Navbar />
-      <PanelButton />
+    <LanguageProvider>
+      <Navbar showPanel={showPanel} />
+      <PanelButton updateState={updateState} />
       <Home />
       <About />
       <Skills />
       <Projects />
       <Contact />
-    </>
+    </LanguageProvider>
   );
 }
 
